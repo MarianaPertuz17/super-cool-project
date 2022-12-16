85cc6c35f2d06219d63464e5fca144ab45e069b9
Mariana <marianapertuzv@gmail.com>
2022-12-16 11:39:25 -0500

    feat: config for the post content displaying

---
+++ src/Post.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Post = React.forwardRef(({post}, ref) => {
+
+  const postBody = (
+    <>
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+      <p>Post ID: {post.id}</p>
+    </>
+  )
+
+  const content = ref 
+    ? <article ref={ref}>{postBody}</article>
+    : <article>{postBody}</article>
+
+  return content;
+})
+
+/*
+why are we doing forwardRef? --> components can't just have refs on them without this. we need a ref because we need to know
+which post is the last one
+*/
+
+export default Post
