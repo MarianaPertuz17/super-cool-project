c1ab7b3927be975ef1c1fd0f27d07721b79fd632
Mariana <marianapertuzv@gmail.com>
2022-12-17 15:44:49 -0500

    feat: added comments for better understanding

--- src/components/PostList/index.jsx
+++ src/components/PostList/index.jsx
@@ -10,32 +10,36 @@ export function PostList() {
 
   const intObserver = useRef(); //NO ENTIENDO ESTO
 
-  console.log(intObserver, 'EL INT OBSERVER  ')
+  /*
+  Con useRef() se está creando un objeto que contiene current que es el valor actual. Con esto se mantiene el 
+  valor de la variable entre cambios de estado. Si se cambia el current, la aplicacion no se va a volver a renderizar 
+  No es como useState que cambia toda la pagina
+  */
 
   const lastPostRef = useCallback(post => {
     if (isLoading) return;
-    if (intObserver.current) intObserver.current.disconnect(); //stop looking if we already have one
+    if (intObserver.current) intObserver.current.disconnect(); //stop looking if we already have one, so that we disconnect the observer until we get a new one.
     
     intObserver.current = new IntersectionObserver(posts => {
-      if (posts[0].isIntersecting && hasNextPage) {
+      if (posts[0].isIntersecting && hasNextPage) { //we want to get the first entry because we observe one by one. isIntersecting means if its on the page somewhere
         setPageNumber(prev => prev + 1 );
         console.log('we are near the last post')
       }
     })
 
-    if (post) intObserver.current.observe(post);
+    if (post) intObserver.current.observe(post); //if we get the last element, then we observe it.
   }, [isLoading, hasNextPage])
 
   if ( isError ) return <p>Error: {error.message}</p>
 
   const content = results.map((post, i) => {
-    if (results.length === i+1) return <Post ref={lastPostRef} key={post.id} post={post}/>
+    if (results.length === i+1) return <Post ref={lastPostRef} key={post.id} post={post}/> //aqui es donde estoy haciendo que el ref se setee al whatever el post sea el que estoy renderizando aqui
     return <Post key={post.id} post={post}/>
   })
 
   return (
     <>
-      <div>Title</div>
+      <div>Title</div>      
       {content}
       {isLoading && <p>Loading more posts...</p>}
       <p><a href='#top'>Back to top</a></p>
