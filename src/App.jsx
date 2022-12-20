b488eed83c6d97f5477c32cd62386b3103cae9d5
Mariana <marianapertuzv@gmail.com>
2022-12-19 21:50:36 -0500

    feat: added router barebones

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import './App.css';
 import PostList from './components/PostList';
-
+import { routes } from './routes';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 
 function App() {
 
   return (
-    <>
-      <PostList/>
-    </>
+    <Router>
+      <Routes>
+        <Route path={routes.POSTS} element={<PostList/>}/>
+      </Routes>
+    </Router>
   );
 }
 
