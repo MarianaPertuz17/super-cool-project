a11c7259d5794bd4d6b691d58f819f23c159a730
Mariana <marianapertuzv@gmail.com>
2022-12-24 15:08:11 -0500

    feat: detail page barebones

--- src/screens/pokemonDashboard/index.jsx
+++ src/screens/pokemonDashboard/index.jsx
@@ -27,18 +27,14 @@ export function PokemonDashboard () {
     })
   }
 
-  const openPokemon = () => {
-
-  }
-
   return (
     <div className={styles.container}>   
       <div className={styles.grid}>
       {poke.length && 
         poke.map((ele, index) => {
           return(
-            <Link to={`pokemon/${ele.name}`}>
-              <button className={styles.card} onClick={openPokemon}>
+            <Link className={styles.card} to={`pokemon/${ele.name}`} state={{ data: ele }}>
+              <button>
                 <img alt='pokemon' style={{width:'30vh', height: '30vh'}} key={index} src={ele.sprites.front_default}/>
                 <h3>{(ele.name).toUpperCase()}</h3> 
               </button>
