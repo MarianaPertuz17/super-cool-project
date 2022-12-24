eb57a049f22526076856461fbbca03e632ce4d0a
Mariana <marianapertuzv@gmail.com>
2022-12-24 09:50:56 -0500

    feat: added screens for movies and posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import './App.css';
-import PostList from './components/PostList';
 import { routes } from './routes';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
 } from 'react-router-dom';
+import { PostsDashboard } from './screens/postsDashboard';
+import { MoviesDashboard } from './screens/moviesDashboard';
 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path={routes.HOME} element={<PostList/>}/>
-        <Route path={routes.POSTS} element={<PostList/>}/>
+        <Route path={routes.HOME} element={<PostsDashboard/>}/>
+        <Route path={routes.POSTS} element={<PostsDashboard/>}/>
+        {/* <Route path={routes.MOVIES} element={<MoviesDashboard/>}/> */}
       </Routes>
     </Router>
   );
