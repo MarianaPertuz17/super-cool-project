edd9c3ebe3ea7116bc5f89dac0b41b81e7c6b2da
Mariana <marianapertuzv@gmail.com>
2022-12-28 12:36:52 -0500

    feat: working with tramullo

--- src/screens/pokemonDashboard/index.jsx
+++ src/screens/pokemonDashboard/index.jsx
@@ -10,33 +10,28 @@ export function PokemonDashboard () {
   const counter = useRef(0);
 
   const scrollToSection = (reference = 0) => {
+    console.log(reference, 'never be me')
     window.scrollTo({
-      top: reference,
+      top: sessionStorage.getItem('scroll-position-pokemon'),
       behavior: "smooth",
     })
-  
+    console.log(reference, 'never be me 2')
+    sessionStorage.setItem("scroll-position-pokemon", 0);
   }
 
 
   useEffect(() => {
     const controller = new AbortController();
     const { signal } = controller;
-    fetchRawPokemon(signal);  
-    console.log('hola')
-    console.log(counter.current, 'antes del dif 0')
-    if (counter.current !== 0) {
-      counter.current = 0;
-      console.log(counter.current, 'despues del dif 0')
-      sessionStorage.setItem("scroll-position-pokemon", 0);  
-    } 
-    
+    fetchRawPokemon(signal);
+    console.log(poke, 'hola')
     return () => controller.abort();
   }, [])
 
-  useEffect(() => {
-    scrollToSection(sessionStorage.getItem('scroll-position-pokemon'));
-    
-  }, [poke])
+  // useEffect(() => {
+  //   scrollToSection(sessionStorage.getItem('scroll-position-pokemon'));
+  // }, [poke])
+
 
   const fetchRawPokemon = async (signal) => {
     const res = await pokemonServices.getRawPokemon(signal);
@@ -45,9 +40,15 @@ export function PokemonDashboard () {
         const fullPokemon = await pokemonServices.getFullPokemon(pokemon.url);
         setPoke(prevState => [...prevState, fullPokemon]);
       })();
-      
     })
+    console.log(poke.length, res.results.length)
+    if (poke.length === res.results.length) scrollToSection(sessionStorage.getItem('scroll-position-pokemon'));
   }
+
+  useEffect(() => {
+    console.log(poke, 'POKE')
+    if (poke.length === 20) scrollToSection(sessionStorage.getItem('scroll-position-pokemon'));
+  }, [poke])
 
   const handleClick = () => {
     const position = window.scrollY;
