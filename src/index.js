fff1c8bdbcde5fe415b4354603ca1e90d3313b4a
Mariana <marianapertuzv@gmail.com>
2022-12-29 18:01:26 -0500

    feat: back to strict mode

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ window.pokemonListCacheMap = new Map();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
+	<React.StrictMode>
 		<App/>
-
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
